76b7476643e232db25ba031f177ba7b20185b0be
Gaston Lesbegueris <gaston.lsber@gmail.com>
2025-03-31 16:29:51 -0300

    remove files

--- cypress/e2e/testcases/main_login.cy.js
+++ cypress/e2e/testcases/main_login.cy.js
@@ -1,4 +1,5 @@
 import MainLoginPage from '../../pages/main_login_page';
+const tools = require('../../../tools.json');
 
 describe('Main Login Tests', () => {
   const loginPage = new MainLoginPage();
@@ -8,17 +9,17 @@ describe('Main Login Tests', () => {
   });
 
   it('test_login_successful_C01', () => {
-    loginPage.login('standard_user', 'secret_sauce');
+    loginPage.login(tools.login.username, tools.login.password);
     cy.url().should('include', '/inventory');
   });
 
   it('test_login_wrong_password_C02', () => {
-    loginPage.login('standard_user', 'wrong_password');
+    loginPage.login(tools.login.username, 'wrong_password');
     loginPage.getErrorMessage().should('contain', 'Username and password do not match');
   });
 
   it('test_login_wrong_username_C03', () => {
-    loginPage.login('wrong_username', 'secret_sauce');
+    loginPage.login('wrong_username', tools.login.password);
     loginPage.getErrorMessage().should('contain', 'Username and password do not match');
   });
 });
